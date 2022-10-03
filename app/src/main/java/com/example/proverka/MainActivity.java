7e0ca112812ca1c4d2a9a8bb57b1212e18710bbe
Ivan <vanomosol@mail.ru>
2022-10-03 15:30:58 +0300

    Maslicov

--- app/src/main/java/com/example/proverka/MainActivity.java
+++ app/src/main/java/com/example/proverka/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
     TextView pass1;
     TextView pass2;
-    TextView text2;
+    TextView email, text2;
+    Toast ts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         pass1 = findViewById(R.id.editTextTextPassword);
         pass2 = findViewById(R.id.editTextTextPassword2);
         text = findViewById(R.id.texttik);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        text2 = findViewById(R.id.textView);
         String str1 = pass1.getText().toString();
         String str2 = pass2.getText().toString();
 
@@ -31,5 +35,16 @@ public class MainActivity extends AppCompatActivity {
         else{
             text.setText("Не равно!");
         }
+        String s = email.getText().toString();
+        if(s.indexOf("@") > 0 && s.matches("[a-z]*@*[a-z,.]*") && s.indexOf(".")>2){
+            ts = Toast.makeText(getApplicationContext(), "Собачка есть, буквы маленькие и отсутствуют спецсимволы!", Toast.LENGTH_SHORT);
+            ts.show();
+        }
+        else {
+            ts = Toast.makeText(getApplicationContext(), "Собачки нет или большие буквы или присутствуют спецсимволы!", Toast.LENGTH_SHORT);
+            ts.show();
+        }
+
+
     }
 }
